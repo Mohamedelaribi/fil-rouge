27bed932836a792393abf73b5b0e2ceb881bf36b
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-06-13 02:45:25 +0100

    up

---
+++ file_roge/navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homePage.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
+</head>
+
+
+<body>
+        <!-- navbar star -->
+        <nav class="header navBar" id="navBar">     
+            
+            <a  class="linkLogo" href="homePage.php"><img class="logo" src="images/Spot.png" alt=""></a>
+        
+                    <div id="divCategory">
+                        <a class="category" id="category" href="homePage.php">Home</a>
+                        <a class="category" id="category" href="AboutUs.html">About Us</a>
+                        <a class="category" id="category" href="Gallery.html">Gallery</a>
+                    </div>
+                    
+                    <div class="divNavBar" id="divNavBar">   
+                            <a class="favoris" id="favoris" href="favorit.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-heart " viewBox="0 0 16 16">
+                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                            </svg>
+                        <button type="submit" name="login" class="login" id="login">
+                            <a href="login.php" id="loginLink">login</a>
+                        </button>   
+                    </div>
+        </nav>
+        <!-- navBar end -->
+</body>
+</html>
---
+++ file_roge/publication.php
@@ -0,0 +1,23 @@
+<?php
+include "navbar.php";
+include "connection.php";
+$selectData="SELECT "
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="publication.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php ?>
+    <section class="section" id="section"></section>
+        <h1 class="bienvenueATanger">Bienvenue à Tanger.</h1>
+           <p class="bienvenueATanger">nous vous aidons à trouver un bon endroit pour passer votre nuit.
+            </p>
+            <img src="images/le mirage 2.jpg" alt="">
+
+</body>
+</html>
